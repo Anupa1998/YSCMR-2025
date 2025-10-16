f13cbc9c7ea9836032e5f3c3da942edd8ab1cf0a
Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>
2025-10-16 11:45:48 +0530

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const milestones = [
     { title: "First Call for Abstracts", date: new Date("May 26, 2025 00:00:00") },
     { title: "Deadline for Abstract Submission", date: new Date("July 14, 2025 23:59:59") },
     { title: "Acceptance Notification", date: new Date("September 15, 2025 23:59:59") },
-    { title: "Registrations Open", date: new Date("October 14, 2025 23:59:59") },
+    { title: "Registrations Open", date: new Date("October 30, 2025 23:59:59") },
     { title: "Conference Date", date: new Date("November 27, 2025 00:00:00") }
   ];
 
